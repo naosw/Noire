cadc8b2f1db6d806130e8045987cce77c6532825
felix18228 <felix18228@hotmail.com>
2023-10-09 04:09:12 -0400

    Scene Update for previous commit
    Scene update after merge for previous commit "Main Loading Screen and First Scene Music BGM"

--- Assets/Scripts/BedRockPlains/Lamp.cs
+++ Assets/Scripts/BedRockPlains/Lamp.cs
@@ -2,13 +2,17 @@
 
 public class Lamp : InteractableObject
 {
+    [SerializeField] private BGMAudio bgmAudio;
     public override void Interact()
     {
         if (CanInteract())
         {
             interactionsOccured++;
             if (!PauseMenu.Instance.IsGamePaused)
+            {
                 GameEventsManager.Instance.BedrockPlainsEvents.LampInteract();
+                bgmAudio.PlayBgmAudio();
+            }
         }
     }
 }
